1e2ea19c7dbe0bad98c03825799031b6664ebaf1
Ayman_Alkom <aymanalkom@gmail.com>
2016-02-26 16:29:54 +0200

    Basic skeleton to start with

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name numPadApp
+ * @description
+ * # numPadApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('numPadApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name numPadApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the numPadApp
+ */
+angular.module('numPadApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
