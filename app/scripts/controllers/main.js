1fc3f35680b7b3a346626001c90fdedcaef36e4e
Ayman_Alkom <aymanalkom@gmail.com>
2016-02-28 00:33:09 +0200

    add numPad service

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,11 +7,9 @@
  * # MainCtrl
  * Controller of the IonicNumPad
  */
-angular.module('IonicNumPad')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+angular.module('IonicNumPad').controller('MainCtrl', function (numPadService) {
+
+	numPadService.open().then(function (num) {
+		console.log(num)
+	});
+});
